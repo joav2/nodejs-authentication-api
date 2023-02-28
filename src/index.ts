f5cd5074a9421660ea858e2a5f449690a5507551
joav2 <joaovitork135@gmail.com>
2023-02-28 14:01:47 -0300

    adicionando rotas

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,18 @@
-console.log("adsadsds");
+import express from "express";
+import statusRoute from "./routes/status.route";
+import usersRoute from "./routes/users.route";
+
+const app = express();
+
+// Configurações da aplicação
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+// Configurações de rotas
+app.use(statusRoute);
+app.use(usersRoute);
+
+// Inicialização do servidor
+app.listen(3000, () => {
+  console.log(`Servidor rodando na porta 3000`);
+});
